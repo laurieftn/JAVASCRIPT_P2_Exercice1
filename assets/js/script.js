566c1a78dd2b16a9a8082d7df06ace0c8e17dae3
laurieftn <laurie.ftn@gmail.com>
2020-11-04 09:43:25 +0100

    Envoi de l'exercice 1 - partie 2

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function NewImage(){
+    document.getElementById("img1").src='assets/img/xps-6uneKLGrJPs-unsplash.jpg'
+}
+
+function OldImage(){
+    document.getElementById("img1").src='assets/img/nathan-dumlao-t2xFUALjMlQ-unsplash.jpg'
+}
